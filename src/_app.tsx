cda4742ad6271e5b8c3b087f9d1ad8ce5d126b64
vineboneto <vineboneto@gmail.com>
2021-12-06 21:53:41 -0300

    feat: create initial layout

--- src/_app.tsx
+++ src/_app.tsx
@@ -1,7 +1,137 @@
+import { useState, ChangeEvent, FormEvent } from 'react'
+import { Wrapper, GlobalStyle } from './styles'
+
 export default function App() {
+  const [state, setState] = useState({
+    m: '',
+    q: '',
+  })
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault()
+  }
+
+  const textChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setState((old) => ({
+      ...old,
+      [e.target.name]: e.target.value,
+    }))
+  }
+
   return (
-    <div>
-      <h1>Turing Machine</h1>
-    </div>
+    <Wrapper>
+      <GlobalStyle />
+      <form onSubmit={handleSubmit}>
+        <div className="main-form">
+          <div className="left-form">
+            <h2 className="title">Valores de Entrada</h2>
+            <input type="text" placeholder="M" name="m" title="M" value={state.m} onChange={textChange} />
+            <input
+              type="text"
+              placeholder="Q"
+              name="q"
+              title="Conjunto de estados internos"
+              value={state.q}
+              onChange={textChange}
+            />
+            <input
+              type="text"
+              placeholder="Γ"
+              name="entrada"
+              title="Alfabeto de entrada"
+              value={state.q}
+              onChange={textChange}
+            />
+            <input
+              type="text"
+              placeholder="Σ"
+              name="fita"
+              title="Alfabeto da fita"
+              value={state.q}
+              onChange={textChange}
+            />
+            <input
+              type="text"
+              placeholder="δ"
+              name="delta"
+              title="Função de Transição"
+              value={state.q}
+              onChange={textChange}
+            />
+            <input
+              type="text"
+              placeholder="q0"
+              name="q0"
+              title="Estado inicial"
+              value={state.q}
+              onChange={textChange}
+            />
+            <input
+              type="text"
+              placeholder="F"
+              name="F"
+              title="Conjunto de estados finais"
+              value={state.q}
+              onChange={textChange}
+            />
+            <input
+              type="text"
+              placeholder="Entrada"
+              name="entrada"
+              title="Entrada"
+              value={state.q}
+              onChange={textChange}
+            />
+            <input
+              type="text"
+              placeholder="Saída"
+              name="saida"
+              title="Saída"
+              value={state.q}
+              onChange={textChange}
+              disabled
+            />
+            <button type="submit">Processar</button>
+          </div>
+          <div className="right-form">
+            <h2 className="title">Máquina de Turing</h2>
+            <input
+              type="text"
+              placeholder="Atual"
+              name="atualEstado"
+              title="Atual Estado"
+              value={state.q}
+              onChange={textChange}
+            />
+            <input
+              type="text"
+              placeholder="Próximo"
+              name="proximoEstado"
+              title="Próximo estado"
+              value={state.q}
+              onChange={textChange}
+            />
+            <input type="text" placeholder="Lê" name="ler" title="Lê" value={state.q} onChange={textChange} />
+            <input
+              type="text"
+              placeholder="Escreve"
+              name="escreve"
+              title="Escreve"
+              value={state.q}
+              onChange={textChange}
+            />
+            <input
+              type="text"
+              placeholder="Direção"
+              name="direction"
+              title="Direção"
+              value={state.q}
+              onChange={textChange}
+            />
+            <button>+</button>
+          </div>
+        </div>
+      </form>
+    </Wrapper>
   )
 }
---
+++ src/styles.ts
@@ -0,0 +1,95 @@
+import styled, { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    font-family: Poppins;
+  }
+`
+
+export const Wrapper = styled.div`
+  height: 100vh;
+  width: 100vw;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  .title {
+    font-size: 25px;
+    user-select: none;
+    font-weight: 300;
+  }
+
+  form {
+    width: 1200px;
+
+    .main-form {
+      display: flex;
+      justify-content: space-between;
+    }
+
+    .left-form {
+      width: 300px;
+    }
+
+    .right-form {
+      width: 850px;
+
+      input {
+        width: 100px;
+        margin-right: 10px;
+      }
+
+      label {
+        flex-wrap: wrap;
+        width: 5px;
+      }
+
+      button {
+        color: purple;
+        margin-top: 0;
+        background-color: #fff;
+        border: 1px purple solid;
+        height: 52px;
+      }
+
+      button:hover {
+        background-color: #fff;
+      }
+    }
+
+    input {
+      padding: 10px 15px;
+      height: 30px;
+      width: 100%;
+      border-radius: 8px;
+      border: 1px solid #ccc;
+      margin: 5px 0;
+    }
+
+    label {
+      font-weight: bold;
+      padding: 5px 5px 20px 5px;
+    }
+
+    button {
+      margin-top: 25px;
+      background-color: purple;
+      color: #fff;
+      border-radius: 8px;
+      padding: 10px 25px;
+      text-transform: uppercase;
+      font-size: 15px;
+      outline: none;
+      border: none;
+      cursor: pointer;
+      font-weight: 400;
+      word-spacing: 30px;
+    }
+
+    button:hover {
+      background-color: #330033;
+    }
+  }
+`
